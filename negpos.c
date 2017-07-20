59eafbc68d8fabe00abc014ced298f8be3a05eac
Yuvpriya <Yuvpriya@users.noreply.github.com>
2017-07-20 13:57:59 +0530

    Create negpos.c

---
+++ negpos.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int n;
+printf("Enter the value:);
+scanf("%d",&n);
+if(n==0){
+printf("Zero");}
+else if(n>0){
+printf("Positive");}
+else {
+printf("Negative");}
+}
